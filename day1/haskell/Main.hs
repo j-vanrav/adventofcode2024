0753a704c40af7de5e0dff275fbfe274078eba5f
j-vanrav <j.vanrav2@gmail.com>
2024-12-02 15:59:01 +0800

    refactor

--- day1/haskell/Main.hs
+++ day1/haskell/Main.hs
@@ -2,7 +2,7 @@ module Main where
 
 import Data.List (sort)
 import Data.List.Split (splitOn)
-import Data.Maybe (fromMaybe)
+import Data.Maybe (mapMaybe)
 import Debug.Trace (trace)
 import System.IO ()
 import Text.Read (readMaybe)
@@ -11,23 +11,21 @@ main :: IO ()
 main = do
   content <- readFile "../input"
   let linesOfFile = lines content
-      parsedLines = fromMaybe [] (mapM parseLine linesOfFile)
+      parsedLines = mapMaybe parseLine linesOfFile
       firstList = sort (map fst parsedLines)
       secondList = sort (map snd parsedLines)
       dist = calcDistance firstList secondList
       sim = calcSimilarity firstList secondList
-  print (show dist ++ ", " ++ show sim)
+  putStrLn (show dist ++ ", " ++ show sim)
 
 calcDistance :: (Num a) => [a] -> [a] -> a
 calcDistance a b = sum (zipWith (\x y -> abs (y - x)) a b)
 
-calcSimilarity :: (Eq a, Num a) => [a] -> [a] -> a
-calcSimilarity a b = sum (zipWithMulti (\x y -> y * numMatches x y) a b)
+calcSimilarity :: [Int] -> [Int] -> Int
+calcSimilarity l1 l2 = sum (zipWith (\x _ -> x * countMatches x l2) l1 l2)
 
-numMatches :: (Eq a, Num a) => [a] -> a -> a
-numMatches [] _ = 0
-numMatches [x] a = if x == a then 1 else 0
-numMatches (x : xs) a = numMatches [x] a + numMatches xs a
+countMatches :: (Eq a) => a -> [a] -> Int
+countMatches a = length . filter (== a)
 
 parseLine :: String -> Maybe (Int, Int)
 parseLine line =
@@ -39,9 +37,6 @@ parseLine line =
     _ -> Nothing
   where
     trim = unwords . words
-
-zipWithMulti :: ([a] -> b -> c) -> [a] -> [b] -> [c]
-zipWithMulti f as = map (f as)
 
 traceVal :: (Show a) => a -> a
 traceVal a = trace ("Trace " ++ show a) a
