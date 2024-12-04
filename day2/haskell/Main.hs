1449cde53c4e104e1a32be4b0e736b7b1e693a6b
j-vanrav <j.vanrav2@gmail.com>
2024-12-04 15:14:48 +0800

    day2p2:haskell + complexity

--- day2/haskell/Main.hs
+++ day2/haskell/Main.hs
@@ -7,9 +7,13 @@ main = do
   content <- readFile "./day2/input"
   let rows = filter (not . null) (fromMaybe [] (mapM ints (lines content)))
   print (length (filter id (map isSafe rows)))
+  print (length (filter id (map isSafeWithDampening rows)))
 
 isSafe :: [Int] -> Bool
-isSafe x = isAdjacencySafe x 0 && (isAscending x || isDescending x)
+isSafe x = isAdjacencySafe x 0
+
+isSafeWithDampening :: [Int] -> Bool
+isSafeWithDampening x = isAdjacencySafeWithDampening x 0
 
 isAdjacencySafe :: [Int] -> Int -> Bool
 isAdjacencySafe [] _ = True
@@ -17,6 +21,13 @@ isAdjacencySafe [x] _ = True
 isAdjacencySafe [x, y] _ = isPairSafe (x, y)
 isAdjacencySafe [x, y, z] _ = isTrebleSafe (x, y, z)
 isAdjacencySafe (x : y : z) i = maybe True isTrebleSafe (toTreble (take 3 (drop i (x : y : z)))) && (((i + 3) > length (x : y : z)) || isAdjacencySafe (x : y : z) (i + 1))
+
+isAdjacencySafeWithDampening :: [Int] -> Int -> Bool
+isAdjacencySafeWithDampening [] _ = True
+isAdjacencySafeWithDampening [x] _ = True
+isAdjacencySafeWithDampening [x, y] _ = isPairSafe (x, y) || isDampenableAt [x, y] 0
+isAdjacencySafeWithDampening [x, y, z] _ = (isTrebleSafe (x, y, z) && (isAscending [x, y, z] || isDescending [x, y, z])) || isDampenableAt [x, y, z] 1
+isAdjacencySafeWithDampening (x : y : z) i = (maybe True isTrebleSafe (toTreble (take 3 (drop i (x : y : z)))) || isDampenableAt (x : y : z) (i + 1)) && (((i + 3) > length (x : y : z)) || isAdjacencySafeWithDampening (x : y : z) (i + 1))
 
 isPairSafe :: (Int, Int) -> Bool
 isPairSafe (x, y) = (x /= y) && (abs (y - x) <= 3)
@@ -27,6 +38,15 @@ toTreble _ = Nothing
 
 isTrebleSafe :: (Int, Int, Int) -> Bool
 isTrebleSafe (x, y, z) = isPairSafe (x, y) && isPairSafe (y, z) && (isAscending [x, y, z] || isDescending [x, y, z])
+
+isDampenableAt :: [Int] -> Int -> Bool
+isDampenableAt as i = any isSafe [deleteAt as (i - 1), deleteAt as i, deleteAt as (i + 1)]
+
+(!?) :: [a] -> Int -> Maybe a
+xs !? i
+  | i < 0 = Nothing
+  | i >= length xs = Nothing
+  | otherwise = Just (xs !! i)
 
 ints :: String -> Maybe [Int]
 ints = mapM readMaybe . words
--- readme.md
+++ readme.md
@@ -12,9 +12,9 @@ My solutions for [advent of code](https://adventofcode.com/) 2024
 1. Install Haskell & Cabal
 2. Run like `cabal run day1` or `ghci ./day1/haskell/Main.hs` + `main`
 
-| Time O() :: Space O() complexity | Go         | Haskell  | Theorised optimum |
-| -------------------------------- | ---------- | -------- | ----------------- |
-| Day 1: Part 1 (distance)         | NlogN :: N | N :: 1   | N :: 1            |
-| Day 1: Part 2 (similarity)       | N^2 :: N   | N^2 :: 1 | NlogN :: 1        |
-| Day 2: Part 1 (safety)           | NM :: NM   | NM :: 1  | NM :: 1           |
-| Day 2: Part 1 (dampening)        | NM :: NM   |          | NM :: 1           |
+| Time O() :: Space O() complexity | Go         | Haskell      | Theorised optimum |
+| -------------------------------- | ---------- | ------------ | ----------------- |
+| Day 1: Part 1 (distance)         | NlogN :: N | N :: 1       | N :: 1            |
+| Day 1: Part 2 (similarity)       | N^2 :: N   | N^2 :: 1     | NlogN :: 1        |
+| Day 2: Part 1 (safety)           | NM :: NM   | NM :: 1      | NM :: 1           |
+| Day 2: Part 2 (dampening)        | NM :: NM   | NM^3 :: NM^2 | NM :: 1           |
