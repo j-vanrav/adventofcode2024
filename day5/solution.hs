9f1918831c0f911abf7b77c69fef663ac2558305
j-vanrav <j.vanrav2@gmail.com>
2024-12-05 22:40:03 +0800

    day 5 part 2 haskell

--- day5/solution.hs
+++ day5/solution.hs
@@ -1,14 +1,17 @@
 module Main where
 
 import Data.Bifunctor (bimap)
+import Data.List (find, sortBy)
 import Data.List.Split (splitOn)
+import Data.Maybe (fromMaybe)
+import Debug.Trace (trace)
 
 main = do
-  content <- readFile "./day5/input"
+  content <- readFile "./day5/sample"
   let (_rs, _ps) = rulesAndPages . words $ content
       (rs, ps) = (map rules _rs, map pages _ps)
 
-  print (orderedSum rs ps)
+  print (sumMiddles (fromMaybe [] (mapM (\uo -> getReorder rs uo []) (unordered rs ps))))
 
 unordered :: [(Int, Int)] -> [[Int]] -> [[Int]]
 unordered rs = filter (\p -> not (isOrdered p rs))
@@ -16,8 +19,11 @@ unordered rs = filter (\p -> not (isOrdered p rs))
 ordered :: [(Int, Int)] -> [[Int]] -> [[Int]]
 ordered rs = filter (`isOrdered` rs)
 
-orderedSum :: [(Int, Int)] -> [[Int]] -> Int
-orderedSum rs ps = sum (map middle (ordered rs ps))
+sumMiddles :: [[Int]] -> Int
+sumMiddles x = sum (map middle x)
+
+getReorder :: [(Int, Int)] -> [Int] -> [Int] -> Maybe [Int]
+getReorder rs psr ps = find (`isOrdered` rs) (allPermutations [] psr)
 
 rulesAndPages :: [String] -> ([String], [String])
 rulesAndPages [x] = if '|' `elem` x then ([x], []) else ([], [x])
@@ -36,6 +42,7 @@ pages x = map read (splitOn "," x)
 isOrdered :: [Int] -> [(Int, Int)] -> Bool
 isOrdered [x, y] rs = checkRules (x, y) rs
 isOrdered (x : ys) rs = all (\y -> checkRules (x, y) rs) ys && isOrdered ys rs
+isOrdered _ _ = True
 
 checkRules :: (Int, Int) -> [(Int, Int)] -> Bool
 checkRules (x, y) [(a, b)] = (x /= b) || (y /= a)
@@ -46,3 +53,17 @@ splitHalf l = splitAt ((length l + 1) `div` 2) l
 
 middle :: [a] -> a
 middle x = last (fst (splitHalf x))
+
+allInserts :: [a] -> a -> [a] -> [[a]]
+allInserts xs y [] = [xs ++ [y]]
+allInserts xs y zs = (xs ++ [y] ++ zs) : allInserts (xs ++ [head zs]) y (tail zs)
+
+allPermutations :: (Eq a, Show a) => [a] -> [a] -> [[a]]
+allPermutations xs [] = [xs]
+allPermutations xs ys = foldMap (\y -> allPermutations (xs ++ [y]) (delete y ys)) ys
+
+delete :: (Eq a) => a -> [a] -> [a]
+delete deleted xs = [x | x <- xs, x /= deleted]
+
+traceVal :: (Show a) => a -> a
+traceVal a = trace ("Trace " ++ show a) a
