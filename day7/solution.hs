eb40e8920b26247a512889e97dbc44efbb267f19
j-vanrav <j.vanrav2@gmail.com>
2024-12-08 13:24:03 +0800

    day 7 part 2 haskell

--- day7/solution.hs
+++ day7/solution.hs
@@ -1,11 +1,15 @@
 module Main where
 
+import Debug.Trace (trace)
+
 main = do
   content <- readFile "./day7/input"
   let problem = map parseLine (lines content)
       solvable = map (\(t, os) -> solve t 0 os) problem
+      solvableWithConcat = map (\(t, os) -> solveWithConcat t 0 os) problem
       part1 = sum (zipWith (\l r -> if r then fst l else 0) problem solvable)
-  print part1
+      part2 = sum (zipWith (\l r -> if r then fst l else 0) problem solvableWithConcat)
+  print (part1, part2)
 
 solve :: Int -> Int -> [Int] -> Bool
 solve target counter [] = target == counter
@@ -13,6 +17,17 @@ solve target counter (o : os) = do
   let _add = counter + o
       _mul = counter * o
   solve target _add os || solve target _mul os
+
+solveWithConcat :: Int -> Int -> [Int] -> Bool
+solveWithConcat target counter [] = target == counter
+solveWithConcat target counter (o : os) =
+  (counter <= target)
+    && ( do
+           let _add = counter + o
+               _mul = counter * o
+               _concat = read (show counter ++ show o)
+           solveWithConcat target _add os || solveWithConcat target _mul os || solveWithConcat target _concat os
+       )
 
 parseLine :: [Char] -> (Int, [Int])
 parseLine ln = (read (init (head ws)), map read (tail ws))
