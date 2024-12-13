efdb803d5e39aa9e38c9ca9dd24b7ef904e36687
j-vanrav <j.vanrav2@gmail.com>
2024-12-13 10:55:01 +0800

    day 9 part 1 haskell solution wip

--- day9/solution.hs
+++ day9/solution.hs
@@ -4,6 +4,11 @@ module Main where
 
 import Data.Function ((&))
 import Data.Maybe (fromMaybe)
+import Data.Vector (Vector)
+import Data.Vector qualified as V
+import Data.Vector.Mutable (MVector)
+import Data.Vector.Mutable qualified as MV
+import Debug.Trace (trace)
 import Text.Read (readMaybe)
 
 main = do
@@ -21,17 +26,20 @@ parseFile :: DiskMap -> File
 parseFile diskMap = concat [if even i then replicate (diskMap !! i) (head (show (i `div` 2))) else replicate (diskMap !! i) '.' | i <- [0 .. length diskMap - 1]]
 
 compressFile :: File -> File
-compressFile file = reverse $ go file []
+compressFile file = reverse $ go (V.fromList (reverse file)) []
   where
-    go [] acc = acc
     go file acc =
       if
-        | last file == '.' -> go (init file) acc
-        | head file == '.' -> go (init $ tail file) (last file : acc)
-        | otherwise -> go (tail file) (head file : acc)
+        | V.length file == 0 -> acc
+        | V.head file == '.' -> go (V.tail file) acc
+        | V.last file == '.' -> go (V.init $ V.tail file) (V.head file : acc)
+        | otherwise -> go (V.init file) (V.last file : acc)
 
 calculateChecksum :: File -> Int
 calculateChecksum file = sum [fromMaybe 0 (readMaybe [file !! i]) * i | i <- [0 .. length file - 1]]
 
 part1 :: String -> Int
 part1 content = content & parseDiskMap & parseFile & compressFile & calculateChecksum
+
+traceVal :: (Show a) => String -> a -> a
+traceVal s a = trace (s ++ show a) a
